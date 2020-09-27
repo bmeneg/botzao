5a5d307d4dbce92509accdcf9a57d9c16381ce57
Bruno Meneguele <bmeneg@heredoc.io>
2020-09-27 19:50:56 -0300

    run: remove daemon code
    Instead of using daemon (which seems to us LSB underneath, which was
    deprecated) lets start it manually as a single thread.  After we prepare things
    for systemd and/or openrc.

--- lib/Tbott/Run.pm
+++ lib/Tbott/Run.pm
@@ -13,14 +13,6 @@ use Tbott::Config;
 our $VERSION = '0.01';
 
 our $tbott = Bot::IRC->new(
-    spawn   => 2,
-    deamon  => {
-        name    => 'tbott_d',
-        lsb_sdesc   => 'Chatbot Tiozao',
-        pid_file    => 'botiozao.pid',
-        stderr_file => 'botiozao.err',
-        stdout_file => 'botiozao.log',
-    },
     connect => {
         server  => 'chat.freenode.net',
         port    => '6697',
