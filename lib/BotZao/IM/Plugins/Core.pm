1c0be1b5df5511eeb3705506e5e0237d7c53f345
Bruno Meneguele <bmeneg@heredoc.io>
2020-10-26 11:54:26 -0300

    im/plugins: pass config to plugins
    Plugins should handle the config file by themselves, so pass it to them.

--- lib/BotZao/IM/Plugins/Core.pm
+++ lib/BotZao/IM/Plugins/Core.pm
@@ -7,14 +7,14 @@ use warnings;
 use feature qw(signatures);
 no warnings qw(experimental::signatures);
 
-use BotZao::Log qw(log_error);
+use BotZao::Log qw(log_info log_error);
 
 # plugin hash = { name => sub_run_ref }
 my %plugins_hash;
 
-sub _init_plugins(@plugins) {
+sub _init_plugins($config, @plugins) {
 	foreach (keys %plugins_hash) {
-		$plugins_hash{$_}() or log_error("failed to load irc plugin $_");
+		$plugins_hash{$_}($config) or log_error("failed to load irc plugin $_");
 	}
 	return;
 }
@@ -26,9 +26,9 @@ sub plugin_add($name, $sub_ref) {
 sub init(%config) {
 	my @plugins;
 
-	_init_config(%config);
 	@plugins = @{$config{im}->{plugins}};
-	_init_plugins(@plugins);
+	log_info(@plugins);
+	_init_plugins(%config, @plugins);
 	return;
 }
 
